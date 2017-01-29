a72b59405db0de1e674718a6a512c2978accbd0d
ScottWBlom <swb013@gmail.com>
2017-01-29 09:11:55 -0800

    created code for 99 - 2 bottles

--- BeerSong.java
+++ BeerSong.java
@@ -8,6 +8,11 @@ public class BeerSong {
   
   public ninety_nine_bottles_of_beer() {
     
+    int x = 99;
+    for (int i = 0; i < 99; i++) {
+      System.out.println(x + " bottles of beer on the wall, " + x + " bottles of beer");
+      System.out.println("take one down, pass it around, " + x-- + " bottles of beer on the wall.");
+    }
     
     
   }
