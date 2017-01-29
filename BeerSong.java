52f09f0b51a8d13a3e26ff77d7a3a2840ba97a50
ScottWBlom <swb013@gmail.com>
2017-01-29 09:13:51 -0800

    added lines for one bottle

--- BeerSong.java
+++ BeerSong.java
@@ -13,7 +13,7 @@ public class BeerSong {
       System.out.println(x + " bottles of beer on the wall, " + x + " bottles of beer");
       System.out.println("take one down, pass it around, " + x-- + " bottles of beer on the wall.");
     }
-    
+    System.out.println("1 bottle of beer on the wall, 1 bottle of beer\n take one down, pass it around, no more bottles of beer on the wall.");
     
   }
 
